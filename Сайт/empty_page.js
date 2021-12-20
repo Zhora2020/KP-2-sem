6c540e84bba3e028b358a34e9cb8802550f04d85
Zhora2020 <45331196+Zhora2020@users.noreply.github.com>
2021-12-20 15:29:54 +0300

    Add files via upload

---
+++ Сайт/empty_page.js
@@ -0,0 +1,9 @@
+ function btn() {
+      alert("Страничка еще не создана");
+    }
+	
+function go_to_vk() {
+	if (confirm("Покинуть сайт и перейти на страницу ВК?")){
+		window.location.href = "https://vk.com/id248665010";
+	}
+}
