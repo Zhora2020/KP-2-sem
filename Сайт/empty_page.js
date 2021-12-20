58850fc26552993dc86940286454a606b084d4d8
Zhora2020 <45331196+Zhora2020@users.noreply.github.com>
2021-12-20 15:32:15 +0300

    Delete Сайт directory

--- Сайт/empty_page.js
+++
@@ -1,9 +0,0 @@
- function btn() {
-      alert("Страничка еще не создана");
-    }
-	
-function go_to_vk() {
-	if (confirm("Покинуть сайт и перейти на страницу ВК?")){
-		window.location.href = "https://vk.com/id248665010";
-	}
-}
